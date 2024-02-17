ec1df3c094cbda09645ffcc844f8bf34fc2ee2a5
com <140906347+YadavAmod@users.noreply.github.com>
2024-02-17 09:21:16 +0530

    Add files via upload
    Node Js GET,POST,PUT and DELETE  API Method

--- api.js
+++ api.js
@@ -1,20 +1,35 @@
 const dbConnect = require('./mongodb');
 const express = require('express');
+const mongodb = require('mongodb');
 const app = express();
 
 app.use(express.json());
-app.get('/',async (res,resp)=>{
-    let data = await dbConnect();
-    data= await data.find().toArray();
-    resp.send(data);
+
+app.get('/', async (req, res) => {
+    let data = await dbConnect(); // assuming dbConnect is a function that returns a collection
+    data = await data.find().toArray();
+    res.send(data);
 });
 
+app.post("/", async (req, res) => {
+    let data = await dbConnect(); // assuming dbConnect is a function that returns a collection
+    let result = await data.insertOne(req.body); // assuming req.body contains the data to be inserted
+    res.send(result);
+});
 
-app.post("/", async (req,resp)=>{
-    let data = await dbConnect();
-    let result = await data.insert(req.body)
-    resp.send(result)
+app.put("/", async (req, res) => {
+    let data = await dbConnect(); // assuming dbConnect is a function that returns a collection
+    let result = await data.updateOne(
+        { name: "Ramesh" },
+        { $set: req.body }
+    );
+    res.send({ result: "update" }); // corrected response format
+});
 
-})
+app.delete("/:id", async (req, res) => {
+    let data = await dbConnect(); // assuming dbConnect is a function that returns a collection
+    let result = await data.deleteOne({ _id: new mongodb.ObjectId(req.params.id) }); // corrected import and usage of ObjectId
+    res.send(result);
+});
 
-app.listen(5000)
+app.listen(5000);
